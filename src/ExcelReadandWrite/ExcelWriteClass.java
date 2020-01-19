5c4f63852d4e3c1f66d359f5426000773a082bb0
Bigtiki <Fhussain781@gmail.com>
2020-01-19 02:01:14 -0500

    Added code for entering data

--- src/ExcelReadandWrite/ExcelWriteClass.java
+++ src/ExcelReadandWrite/ExcelWriteClass.java
@@ -1,12 +1,19 @@
 package ExcelReadandWrite;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.apache.poi.hssf.model.InternalWorkbook;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelWriteClass {
@@ -15,7 +22,8 @@ public class ExcelWriteClass {
 		// TODO Auto-generated method stub
 		
 		  System.out.println("Writing file");
-		  ExcelWrite();
+		  //ExcelWrite();
+		  EnterData();
 		 
 		
 	}
@@ -68,4 +76,33 @@ public class ExcelWriteClass {
 			System.out.println("File Closed");
 		}
 
+		public static void EnterData() {
+			try {
+				FileInputStream EnterMyInfo = new FileInputStream(FileX);
+				XSSFWorkbook Wb= new XSSFWorkbook(EnterMyInfo);
+				XSSFSheet Sh= Wb.getSheetAt(0);
+				XSSFRow Row1= Sh.createRow(6);
+				XSSFCell Cell1=Row1.createCell(0);
+				Cell1.setCellValue("EXcel Works");
+				XSSFCell Cell2=Row1.createCell(1);
+				Cell2.setCellValue("01-11-2019");
+				XSSFCell Cell3=Row1.createCell(2);
+				Cell3.setCellValue("01-12-2019");
+				XSSFCell Cell4=Row1.createCell(3);
+				Cell4.setCellValue("NA");
+				EnterMyInfo.close();
+				FileOutputStream EndMyInfo = new FileOutputStream(FileX);
+				Wb.write(EndMyInfo);
+				Wb.close();
+				System.out.println("Update Completed");
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch blockXf
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+		}
 }
